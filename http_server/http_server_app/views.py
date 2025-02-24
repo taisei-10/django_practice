948a21ee5611f9aab2d1c530e57ec6f8f2eb52f6
t.awaji <awaji_taisei@awajiyasunarinoMacBook-Pro.local>
2025-02-24 18:40:40 +0900

    add html templates

--- http_server/http_server_app/views.py
+++ http_server/http_server_app/views.py
@@ -3,5 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+# def index(request):
+#     return HttpResponse("Hello World!!!")
+
 def index(request):
-    return HttpResponse("Hello World")
+    my_dict={
+        'insert_something':"views.pyのinsert_something部分です。",
+        'name':'yasunari',
+        'test_titles':["title 1","title 2","title 3"],
+    }
+    return render(request, 'httpserverapp/index.html',my_dict)
