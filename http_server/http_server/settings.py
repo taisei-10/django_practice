1aba2e7e5973673c25f22aebef6865f9ab85c706
t.awaji <awaji_taisei@awajiyasunarinoMacBook-Pro.local>
2025-03-03 21:25:52 +0900

    add database function

--- http_server/http_server/settings.py
+++ http_server/http_server/settings.py
@@ -11,12 +11,18 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_DIR = Path.joinpath(BASE_DIR, 'static')
 
-
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -77,10 +83,7 @@ WSGI_APPLICATION = 'http_server.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(),
 }
 
 
