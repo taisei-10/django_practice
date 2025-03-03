7493e500540b9f135c956fcc1bff8fd561e3ec86
t.awaji <awaji_taisei@awajiyasunarinoMacBook-Pro.local>
2025-03-03 21:08:40 +0900

    add database function

--- http_server/http_server_app/urls.py
+++ http_server/http_server_app/urls.py
@@ -5,4 +5,8 @@ app_name='http_server_app'
 
 urlpatterns = [
     path('test', views.index, name='index'),
+    path('info', views.info, name='info'),
+    path('create', views.create, name='create'),
+    path('update/<int:num>', views.update, name='update'),
+    path('delete/<int:num>', views.delete, name='delete')
 ]
--- http_server/http_server_app/views.py
+++ http_server/http_server_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .forms import TestForm
+from .models import Employee
+from .forms import EmployeeAdd
+from django.shortcuts import redirect
 # Create your views here.
 
 # def index(request):
@@ -11,5 +14,75 @@ def index(request):
         'insert_something':"views.pyのinsert_something部分です。",
         'name':'yasunari',
         'test_titles':["title 1","title 2","title 3"],
+        'form': TestForm(),
+        'insert_forms': '初期値',
     }
+
+    if (request.method == 'POST'):
+        my_dict['insert_forms'] = '文字列:' + request.POST['text'] + '\n整数型:' + request.POST['num']
+        my_dict['form'] = TestForm(request.POST)
+    
     return render(request, 'httpserverapp/index.html',my_dict)
+
+def info(request):
+    employees = Employee.objects.all()
+    header = ['ID','名前','メール','性別','部署','社歴','作成日']
+    my_dict2 = {
+        'title':'テスト',
+        'employees': employees,
+        'header': header,
+    }
+
+    return render(request, 'httpserverapp/info.html', my_dict2)
+
+
+def create(request):
+
+    message = ''
+    if (request.method == 'POST'):
+        form = EmployeeAdd(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='/info')
+        else:
+            message = '再入力してください'
+    modelform_dict = {
+        'title': 'modelformテスト',
+        'form': EmployeeAdd(),
+        'message':message,
+    }
+    return render(request, 'httpserverapp/create.html', modelform_dict)
+
+def update(request, num):
+    message = ''
+    employee_obj = Employee.objects.get(id=num)
+    if (request.method == 'POST'):
+        employee = EmployeeAdd(request.POST, instance=employee_obj)
+        if employee.is_valid():
+            employee.save()
+            return redirect(to='/info')
+        else:
+            message = '再入力してください'
+    update_dict = {
+        'title': '登録情報更新画面',
+        'id': num,
+        'form': EmployeeAdd(instance=employee_obj),
+        'message':message,
+    }
+    return render(request, 'httpserverapp/update.html', update_dict)
+
+def delete(request, num):
+    header = ['ID','名前','メール','性別','部署','社歴','作成日']
+    message = ''
+    employee_obj = Employee.objects.get(id=num)
+    if (request.method == 'POST'):
+        employee_obj.delete()
+        return redirect(to='/info')
+    delete_dict = {
+        'title': '削除画面',
+        'header': header,
+        'id': num,
+        'employee': employee_obj,
+        'message': message,
+    }
+    return render(request, 'httpserverapp/delete.html', delete_dict)
